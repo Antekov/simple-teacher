0bc326e9d5139a7ed6d09632dbd35b671e383fd5
Mike Antekov <antekov@yandex.ru>
2019-08-12 13:18:25 +0300

    Design

--- application/views/main.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-$this->load->view('header', $this->stash);
-
-?>
-MAIN
-<?php
-$this->load->view('footer');
-?>
--- application/views/main/index.php
+++ application/views/main/index.php
@@ -3,24 +3,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $this->load->view('header', $this->stash);
 ?>
 <div class="body-container">
-	<div class="content j-content">
+    <div class="content j-content">
+        <div style="">
+            <div class="row">
+                <div class="col-sm-8 col-sm-offset-2 jumbotron">
 
-		<!-- Main component for a primary marketing message or call to action -->
-		<div class="jumbotron">
-			<h1>MAIN Navbar example</h1>
+                    <!-- Main component for a primary marketing message or call to action -->
+                    <div class="row">
+                        <div class="col-xs-10 col-xs-offset-1 ta-center">
+                            <h1>Просто-репетитор</h1>
 
-			<p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It
-				includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+                            <p>Расписание и ученики</p>
 
-			<p>To see the difference between static and fixed top navbars, just scroll.</p>
-
-			<p>
-				<a class="btn btn-lg btn-primary" href="/dashboard/" role="button">Dashboard »</a>
-				<a class="btn btn-lg btn-default" href="/login/" role="button">Login »</a>
-			</p>
-		</div>
-
-	</div>
+                            <p>
+                                <a class="btn btn-lg btn-primary col-xs-8 col-xs-offset-2" href="/dashboard/" role="button">Открыть</a>
+                                
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php
